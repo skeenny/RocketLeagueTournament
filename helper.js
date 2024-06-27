bdf17f499f51ba64a5f94bef35d54438f475abf1
Andrii Piatnytskyi <apiatnytskyi@jappware.com>
2024-06-27 22:51:45 +0300

    Added info page

--- helper.js
+++ helper.js
@@ -4,6 +4,11 @@ if (location.href.includes('teams')) {
   const menuItem2 = document.getElementById('img-teams-left');
   menuItem1.className = `${menuItem1.className} active`;
   menuItem2.className = `${menuItem2.className} active`;
+} else if (location.href.includes('info')) {
+  const menuItem1 = document.getElementById('img-info');
+  const menuItem2 = document.getElementById('img-info-left');
+  menuItem1.className = `${menuItem1.className} active`;
+  menuItem2.className = `${menuItem2.className} active`;
 } else {
   const menuItem1 = document.getElementById('img-squares');
   const menuItem2 = document.getElementById('img-squares-left');
