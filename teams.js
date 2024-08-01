6533d33745b1ba78ec305501023ce1009c5e401c
Andrii Piatnytskyi <apiatnytskyi@jappware.com>
2024-08-01 14:05:01 +0300

    Added matches

--- teams.js
+++ teams.js
@@ -1,4 +1,4 @@
-let activeTour = 4;
+let activeTour = 5;
 document.getElementById(`tourN-${activeTour}`).className = 'tour-button tour-active';
 function getRankImage(rank) {
   const rankImages = [
