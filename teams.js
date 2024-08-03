43c0daa33be9fa0a72229504060a0720fd8b3b04
Andrii Piatnytskyi <apiatnytskyi@jappware.com>
2024-08-03 18:38:39 +0300

    Htofix

--- teams.js
+++ teams.js
@@ -1,4 +1,4 @@
-let activeTour = 5;
+let activeTour = 6;
 document.getElementById(`tourN-${activeTour}`).className = 'tour-button tour-active';
 function getRankImage(rank) {
   const rankImages = [
