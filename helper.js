9f2096fb9c087ba1c014c0e401495d9766dc377a
Andrii Piatnytskyi <apiatnytskyi@jappware.com>
2024-08-05 18:18:04 +0300

    Added playoff page

--- helper.js
+++ helper.js
@@ -9,6 +9,11 @@ if (location.href.includes('teams')) {
   const menuItem2 = document.getElementById('img-info-left');
   menuItem1.className = `${menuItem1.className} active`;
   menuItem2.className = `${menuItem2.className} active`;
+} else if (location.href.includes('playoff')) {
+  const menuItem1 = document.getElementById('img-cup');
+  const menuItem2 = document.getElementById('img-cup-left');
+  menuItem1.className = `${menuItem1.className} active`;
+  menuItem2.className = `${menuItem2.className} active`;
 } else {
   const menuItem1 = document.getElementById('img-squares');
   const menuItem2 = document.getElementById('img-squares-left');
